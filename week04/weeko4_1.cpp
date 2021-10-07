20695e39e43847c8edcb32ca39122b2a19dea64c
iamnotCreeper <91250406+iamnotCreeper@users.noreply.github.com>
2021-10-07 17:37:17 +0800

    Add files via upload

---
+++ week04/weeko4_1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float c=3.14159265358979;
+    double b=3.14159265358979;
+    printf("%f\n",c);
+    printf("%f\n",b);
+    printf("%.4f\n",b);
+    printf("%.9f\n",b);
+    printf("%f.13\n",b);
+}
