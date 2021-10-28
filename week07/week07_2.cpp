ac027755adfacfdcd2b1864e33f3f95202aa1438
iamnotCreeper <91250406+iamnotCreeper@users.noreply.github.com>
2021-10-28 17:39:51 +0800

    Add files via upload

---
+++ week07/week07_2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n=3;
+
+    if(n>0){
+        printf("n大於0喔\n");
+    }
+    while(n>0){
+        printf("while(判斷a>0)有進來喔\n");
+    }
+}
---
+++ week07/week07_3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n=3;
+
+    while(n>0){
+        printf("while(§PÂ_a>0)¦³¶i¨Ó³á,n:%d\n",n);
+
+        n--;
+    }
+}
